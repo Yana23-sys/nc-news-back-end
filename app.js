c12626fa570539b8b74662c08e8f4d55b81e6b2a
Yana Turiak <turyak1998@gmail.com>
2024-07-15 15:52:16 +0100

    GET /api/articles/:article_id endpoint and tests

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require("express")
 const app = express()
-const {getTopics} = require('./topics')
-const {serverErrorHandler} = require('./error-handlers')
+const {psqlErrorHandler, customErrorHandler, serverErrorHandler} = require('./error-handlers')
+
 const {getEndpoints} = require('./endpoints/controllers')
+const {getTopics} = require('./topics')
+const {getArticleById} = require('./articles')
 
 
 app.get('/api', getEndpoints)
 
+
 app.get("/api/topics", getTopics)
+
+
+app.get('/api/articles/:article_id', getArticleById)
 
 
 app.all('*', (request, response, next) => {
     response.status(404).send({msg: 'path not found'})
 })
   
+app.use(psqlErrorHandler)
+app.use(customErrorHandler)
 app.use(serverErrorHandler)
 
 module.exports = app;
